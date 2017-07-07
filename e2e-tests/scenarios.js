b6face680c851abe016e531eb25fd98e9cf95fb3
Dyesse YUMBA NDUBA <yumba.nduba@gmail.com>
2017-07-07 18:06:49 +0100

    Revert "Added gulp-eslint."
    This reverts commit 133ae64cb9e9eec0b30780e0f98079a05501c4d0.

--- e2e-tests/scenarios.js
+++
@@ -1,7 +0,0 @@
-describe('Music Search Demo App', function() {
-  it('should have a title', function() {
-    browser.get('http://localhost:3001');
-
-    expect(browser.getTitle()).toEqual('Home - Music Search');
-  });
-});
